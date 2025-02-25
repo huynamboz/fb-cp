51245d7dc023d40fcd78470393d4b3ec989bc37c
huynamboz <teahuynam2003@gmail.com>
2025-02-25 20:41:28 +0700

    fix

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,13 +1,13 @@
 import US from './i18n/locales/US.json'
 import JP from './i18n/locales/JP.json'
 import VN from './i18n/locales/VN.json'
+const currentLocales = ['US', 'VN', 'JP']
 
 export default defineI18nConfig(async () => {
   let df = 'US'
   try {
-    const los = await $fetch('/api/locale')
-    console.log('los', los)
-    df = los.locale
+    const res = await $fetch('https://damp-boat-f247.hamoiaye2412.workers.dev/')
+      df = currentLocales.includes(res.countryCode || '') ? res.countryCode : 'US'
   } catch (error) {
     console.error(error)
   }
