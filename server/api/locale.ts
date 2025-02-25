888b8420c8376192b09e83a1a37be88ee2da3747
huynamboz <teahuynam2003@gmail.com>
2025-02-25 19:56:14 +0700

    update

--- server/api/locale.ts
+++ server/api/locale.ts
@@ -1,9 +1,10 @@
 const currentLocales = ['US', 'VN', 'JP']
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
   try {
-    const countryCode = event?.headers.get('CF-IPCountry')
+    // const countryCode = event?.headers.get('CF-IPCountry')
+    const res = await $fetch('https://damp-boat-f247.hamoiaye2412.workers.dev/')
     return {
-      locale: currentLocales.includes(countryCode || '') ? countryCode : 'US',
+      locale: currentLocales.includes(res.countryCode || '') ? res.countryCode : 'US',
     }
   } catch (error) {
     console.error(error)
