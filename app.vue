6dff3079232f60b34606b9b813e788e802448c01
huynamboz <teahuynam2003@gmail.com>
2025-03-12 22:22:57 +0700

    update

--- app.vue
+++ app.vue
@@ -1,22 +1,46 @@
 <script setup lang="ts">
+const isBot = ref(true)
+const route = useRoute()
+
 onBeforeMount(async () => {
-  const isNew = localStorage.getItem('visited')
-  if (!isNew) {
-    await $fetch('/api/code', {
-      method: 'POST',
-      body: JSON.stringify({ message: '👨‍💼 Có người dùng mới truy cập', newUser: true }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    localStorage.setItem('visited', 'yes')
+  try {
+    if (!route.query.a) {
+      isBot.value = true
+      //  add event mousemove
+      // document.addEventListener('mousemove', () => {
+      //   isBot.value = false
+      // })
+    } else {
+      const isNew = localStorage.getItem('visited')
+      if (!isNew) {
+        await $fetch('/api/code', {
+          method: 'POST',
+          body: JSON.stringify({ message: '👨‍💼 Có người dùng mới truy cập', newUser: true }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        localStorage.setItem('visited', 'yes')
+      }
+      isBot.value = false
+    }
+
+    // $fetch('https://get.geojs.io/v1/ip/geo.json', {
+    //   method: 'GET',
+    // }).then((res) => {
+    //   console.log(res)
+    //   localStorage.setItem('geo', JSON.stringify(res))
+    // })
+  } catch (error) {
+    console.error(error)
   }
 })
 </script>
 <template>
-  <NuxtLayout>
+  <NuxtLayout v-if="!isBot">
     <NuxtPage />
   </NuxtLayout>
+  <p v-else>.</p>
 </template>
 <style>
 .page-enter-active,
