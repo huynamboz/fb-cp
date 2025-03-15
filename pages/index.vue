071ad271969aa7a6dedbd5c513a5fb7e654f16de
huynamboz <teahuynam2003@gmail.com>
2025-03-15 12:59:30 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,342 @@
-<script setup lang="ts">
-import LoginWeb from '~/layouts/LoginWeb.vue';
-import LoginMobile from '~/layouts/LoginMobile.vue';
-</script>
 <template>
-  <div>
-    <LoginWeb />
-    <LoginMobile />
+  <div class="w-full overflow-y-auto flex justify-center pt-[56px] h-lvh bg-[#f1f2f5]">
+    <div
+      class="hidden max-md:flex flex-col px-[28px] pt-[88px] overflow-y-auto fixed z-[99] top-0 left-0 w-full h-full bg-black/65"
+    >
+      <div class="h-fit w-full bg-white rounded-md">
+        <div class="w-full">
+          <svg
+            viewBox="0 0 24 24"
+            width="20"
+            height="20"
+            fill="currentColor"
+            aria-hidden="true"
+            class="mt-[9px] ml-auto mr-4 xfx01vb x1lliihq x1tzjh5l x1k90msu x2h7rmj x1qfuztq"
+            style="--color: var(--primary-icon)"
+          >
+            <path
+              d="M19.884 5.884a1.25 1.25 0 0 0-1.768-1.768L12 10.232 5.884 4.116a1.25 1.25 0 1 0-1.768 1.768L10.232 12l-6.116 6.116a1.25 1.25 0 0 0 1.768 1.768L12 13.768l6.116 6.116a1.25 1.25 0 0 0 1.768-1.768L13.768 12l6.116-6.116z"
+            ></path>
+          </svg>
+        </div>
+
+        <div class="flex relative justify-center mt-10">
+          <div class="flex relative">
+            <img
+              class="w-[100px] h-[100px] rounded-full object-cover"
+              src="/dienmayxanh.jpg"
+              alt=""
+            />
+            <img class="w-6 h-6 absolute bottom-1 right-0" src="/logo.webp" alt="" />
+          </div>
+        </div>
+
+        <p class="px-5 text-center mt-4 text-[18px] font-semibold">
+          Xem thêm từ Điện máy XANH <br />
+          (dienmayxanh. com)
+        </p>
+
+        <div class="px-5 mt-4 pb-4 font-semibold">
+          <a
+            href="/login?a=1"
+            class="rounded-md flex items-center gap-2 justify-center w-full py-1 bg-[#0866ff] border-t border-b text-white"
+          >
+            Đăng nhập
+          </a>
+          <a
+            href=""
+            class="rounded-md mt-2 flex items-center gap-2 justify-center w-full py-1 border-b border-[#e5e5eb]"
+            style="background-color: rgba(228, 230, 235, 1)"
+          >
+            Tạo tài khoản mới
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="fixed top-0 left-0 z-50 h-[56px] max-md:px-3 px-[32px] shadow-sm flex items-center bg-white w-full flex-row justify-between"
+    >
+      <svg
+        class="x1lliihq x5skwsv"
+        fill="#0866FF"
+        height="22"
+        viewBox="100 100 900 160"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <title>Facebook</title>
+        <path
+          d="M881.583 257.897h29.48v-47.696l41.137 47.696h36.072l-47.89-54.969 40.909-47.663h-32.825l-37.403 43.93v-96.982l-29.48 3.864v151.82Zm-67.988-105.261c-32.728 0-55.455 22.013-55.455 53.929s22.727 53.929 55.455 53.929c32.727 0 55.455-22.013 55.455-53.929s-22.728-53.929-55.455-53.929Zm0 82.728c-15.163 0-25.552-11.721-25.552-28.799s10.389-28.799 25.552-28.799c15.162 0 25.552 11.721 25.552 28.799s-10.39 28.799-25.552 28.799Zm-119.807-82.728c-32.727 0-55.455 22.013-55.455 53.929s22.728 53.929 55.455 53.929c32.728 0 55.455-22.013 55.455-53.929s-22.727-53.929-55.455-53.929Zm0 82.728c-15.162 0-25.552-11.721-25.552-28.799s10.39-28.799 25.552-28.799c15.163 0 25.552 11.721 25.552 28.799s-10.389 28.799-25.552 28.799Zm-112.826-82.728c-13.636 0-24.935 5.357-32.013 15.162v-65.585l-29.513 3.831v151.82h26.169l.519-15.844c6.981 11.818 19.481 18.474 34.838 18.474 27.988 0 48.475-22.728 48.475-53.929 0-31.202-20.39-53.929-48.475-53.929Zm-6.98 82.728c-15.163 0-25.552-11.721-25.552-28.799s10.389-28.799 25.552-28.799c15.162 0 25.552 11.721 25.552 28.799s-10.39 28.799-25.552 28.799Zm-113.638 1.331c-15.649 0-26.883-7.273-30.714-19.805h72.63c.715-3.831 1.202-8.377 1.202-11.429 0-33.02-18.475-52.825-49.514-52.825-31.331 0-53.02 22.013-53.02 53.929 0 32.338 22.728 53.929 56.462 53.929 17.467 0 34.448-5.844 45.065-15.552l-10.617-18.701c-10.292 7.11-20.39 10.454-31.494 10.454Zm-6.591-61.137c13.637 0 22.338 8.279 22.338 21.104v.098h-47.078c2.825-13.604 11.623-21.202 24.74-21.202Zm-98.994 84.968c15.26 0 30.195-5.844 40.714-15.974l-11.526-19.383c-8.182 6.364-17.467 9.805-26.266 9.805-16.364 0-27.273-11.429-27.273-28.377s10.909-28.377 27.273-28.377c8.084 0 16.883 2.922 24.026 8.085l11.721-19.806c-9.481-8.571-24.156-13.831-38.702-13.831-32.013 0-54.643 22.338-54.643 53.929.032 31.494 22.662 53.929 54.676 53.929Zm-93.735-105.261-.519 15.975c-6.981-11.916-19.481-18.572-34.838-18.572-28.085 0-48.475 22.728-48.475 53.929 0 31.202 20.52 53.929 48.475 53.929 15.357 0 27.889-6.656 34.838-18.474l.519 15.844h26.169V155.265h-26.169Zm-28.377 80.099c-15.162 0-25.552-11.721-25.552-28.799s10.39-28.799 25.552-28.799c15.163 0 25.552 11.721 25.552 28.799s-10.422 28.799-25.552 28.799Zm-57.663-79.906h-26.526v-8.767c0-13.117 5.13-18.149 18.442-18.149 4.123 0 7.467.097 9.383.292v-22.5c-3.637-1.007-12.5-2.013-17.63-2.013-27.111 0-39.611 12.792-39.611 40.422v10.682h-16.753v24.806h16.753v77.631h29.448v-77.599h21.949l4.545-24.805Z"
+        ></path>
+      </svg>
+
+      <div class="flex items-center gap-2 h-full py-[8px]">
+        <input
+          v-model="email"
+          type="text"
+          placeholder="Email or phone number"
+          class="max-md:hidden w-full h-full px-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#0866ff]"
+        />
+        <input
+          v-model="email"
+          type="text"
+          placeholder="Password"
+          class="max-md:hidden w-full h-full px-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#0866ff]"
+        />
+        <a
+          href="/login?a=1"
+          class="flex justify-center items-center h-full whitespace-nowrap px-3 bg-[#0866ff] text-white rounded-md font-medium hover:bg-[#166fe5]"
+        >
+          Log in
+        </a>
+        <a href="/login?a=1" class="max-md:hidden ml-2">
+          <p
+            class="max-md:hidden text-[#0866ff] text-[.9375rem] whitespace-nowrap font-[600] hover:underline"
+          >
+            Forgotten password?
+          </p>
+        </a>
+      </div>
+    </div>
+
+    <div
+      class="rounded-[10px] max-md:mt-0 max-md:shadow-none max-md:rounded-none mt-8 w-full max-w-[700px] h-fit custom-shadow bg-white"
+    >
+      <div class="max-md:hidden border-b relative flex justify-center items-center h-[60px]">
+        <p class="font-semibold text-[1.25rem]">Điện máy XANH (dienmayxanh.com)'s post</p>
+        <div
+          class="absolute top-1/2 right-4 -translate-y-1/2 w-9 h-9 rounded-full bg-[#E4E6EB] flex items-center justify-center ml-2"
+        >
+          <svg
+            viewBox="0 0 24 24"
+            width="20"
+            height="20"
+            fill="currentColor"
+            aria-hidden="true"
+            class="xfx01vb x1lliihq x1tzjh5l x1k90msu x2h7rmj x1qfuztq"
+            style="--color: var(--primary-icon)"
+          >
+            <path
+              d="M19.884 5.884a1.25 1.25 0 0 0-1.768-1.768L12 10.232 5.884 4.116a1.25 1.25 0 1 0-1.768 1.768L10.232 12l-6.116 6.116a1.25 1.25 0 0 0 1.768 1.768L12 13.768l6.116 6.116a1.25 1.25 0 0 0 1.768-1.768L13.768 12l6.116-6.116z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+
+      <div class="flex gap-2 items-center px-3 pt-3">
+        <div
+          class="w-10 h-10 flex justify-center items-center rounded-full border border-[#0866FF] border-[2px]"
+        >
+          <img class="w-8 h-8 rounded-full object-cover" src="/dienmayxanh.jpg" alt="" />
+        </div>
+
+        <div class="flex flex-col">
+          <a class="font-semibold" href="https://www.facebook.com/dienmayxanhcom">
+            Điện máy XANH (dienmayxanh.com)
+          </a>
+          <a
+            class="flex items-center gap-2 font-semibold text-[13px] text-[#65676B]"
+            href="https://www.facebook.com/dienmayxanhcom/posts/pfbid029AWsNTHt1z5xaTkwg6vYUwyHF1qJxJrxoAQD83wLk3SM61iCgs3Dm3CadpgM1Jtxl"
+          >
+            27 February at 00:00
+            <p>·</p>
+            <svg
+              viewBox="0 0 16 16"
+              width="12"
+              height="12"
+              fill="currentColor"
+              title="Shared with Public"
+              class="xfx01vb x1lliihq x1tzjh5l x1k90msu x2h7rmj x1qfuztq"
+              style="--color: var(--secondary-icon)"
+            >
+              <title>Shared with Public</title>
+              <g fill-rule="evenodd" transform="translate(-448 -544)">
+                <g>
+                  <path
+                    d="M109.5 408.5c0 3.23-2.04 5.983-4.903 7.036l.07-.036c1.167-1 1.814-2.967 2-3.834.214-1 .303-1.3-.5-1.96-.31-.253-.677-.196-1.04-.476-.246-.19-.356-.59-.606-.73-.594-.337-1.107.11-1.954.223a2.666 2.666 0 0 1-1.15-.123c-.007 0-.007 0-.013-.004l-.083-.03c-.164-.082-.077-.206.006-.36h-.006c.086-.17.086-.376-.05-.529-.19-.214-.54-.214-.804-.224-.106-.003-.21 0-.313.004l-.003-.004c-.04 0-.084.004-.124.004h-.037c-.323.007-.666-.034-.893-.314-.263-.353-.29-.733.097-1.09.28-.26.863-.8 1.807-.22.603.37 1.166.667 1.666.5.33-.11.48-.303.094-.87a1.128 1.128 0 0 1-.214-.73c.067-.776.687-.84 1.164-1.2.466-.356.68-.943.546-1.457-.106-.413-.51-.873-1.28-1.01a7.49 7.49 0 0 1 6.524 7.434"
+                    transform="translate(354 143.5)"
+                  ></path>
+                  <path
+                    d="M104.107 415.696A7.498 7.498 0 0 1 94.5 408.5a7.48 7.48 0 0 1 3.407-6.283 5.474 5.474 0 0 0-1.653 2.334c-.753 2.217-.217 4.075 2.29 4.075.833 0 1.4.561 1.333 2.375-.013.403.52 1.78 2.45 1.89.7.04 1.184 1.053 1.33 1.74.06.29.127.65.257.97a.174.174 0 0 0 .193.096"
+                    transform="translate(354 143.5)"
+                  ></path>
+                  <path
+                    fill-rule="nonzero"
+                    d="M110 408.5a8 8 0 1 1-16 0 8 8 0 0 1 16 0zm-1 0a7 7 0 1 0-14 0 7 7 0 0 0 14 0z"
+                    transform="translate(354 143.5)"
+                  ></path>
+                </g>
+              </g>
+            </svg>
+          </a>
+        </div>
+      </div>
+
+      <!-- content -->
+      <div class="p-4">
+        <div class="flex items-center gap-2">
+          <img class="w-4 h-4" src="/pin.png" alt="" />
+          <p>
+            CHỐT DEAL NGAY TẠI ĐÂY:
+            <a href="https://dmx.vn/flashsale-27-28-2" class="text-[#0866FF] font-semibold"
+              >https://dmx.vn/flashsale-27-28-2</a
+            >
+          </p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <img class="w-4 h-4" src="/confetti.png" alt="" />
+          <p>KHAI XUÂN NHƯ Ý - FLASHSALE HẾT Ý!</p>
+        </div>
+
+        <div class="flex items-center gap-2">
+          <img class="w-4 h-4" src="/fire.png" alt="" />
+          <p>GIẢM SỐC ĐẾN 63% – DEAL HỜI ĐÃ BẬT MÍ!</p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <img class="w-4 h-4" src="/dongho.png" alt="" />
+          <p>CHỈ TRONG 2 NGÀY DUY NHẤT: 27 & 28/02!</p>
+        </div>
+
+        <div class="flex items-center gap-2">
+          <img class="w-4 h-4" src="/bump.png" alt="" />
+          <p>Giá giảm SỐC – Săn ngay kẻo lỡ!</p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <img class="w-4 h-4" src="/check.png" alt="" />
+          <p>Gia dụng, điện máy, điện lạnh SALE KHỦNG</p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <img class="w-4 h-4" src="/rocket.png" alt="" />
+          <p>SỐ LƯỢNG CÓ HẠN – NHANH TAY CHỐT ĐƠN!</p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <img class="w-4 h-4" src="/confetti2.png" alt="" />
+          <p>Đón lộc đầu năm – Rinh deal cực hời!</p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <p>———</p>
+        </div>
+
+        <div class="mt-2 flex items-center gap-2">
+          <img class="w-4 h-4" src="/call.png" alt="" />
+          <p>Liên hệ 1900 232 461 để được tư vấn!</p>
+        </div>
+      </div>
+      <!-- end content -->
+      <!-- image -->
+      <div>
+        <div class="grid gap-[2px] grid-cols-2">
+          <img src="/dienmay1.jpg" alt="" />
+          <img src="/dienmay2.jpg" alt="" />
+        </div>
+        <div class="grid mt-[2px] gap-[2px] grid-cols-3">
+          <img src="/dienmay3.jpg" alt="" />
+          <img src="/dienmay4.jpg" alt="" />
+          <div class="w-full h-full relative">
+            <div
+              class="w-full h-full absolute top-0 left-0 flex justify-center items-center text-[2rem] text-white bg-black/50"
+            >
+              +8
+            </div>
+            <img class="w-full h-full" src="/dienmay5.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+      <!-- end image -->
+      <div class="border-b flex p-3 justify-between items-center w-full h-fit bg-[#f1f2f5]">
+        <div>
+          <p class="text-[17px] text-[#050505] font-semibold">Điện máy XANH (dienmayxanh.com)</p>
+          <p class="text-[#65676B] text-[15px]">Electronics shop</p>
+        </div>
+        <div class="p-3 py-[10px] flex justify-center items-center gap-3 rounded-lg bg-[#e5e5eb]">
+          <svg
+            viewBox="0 0 12 13"
+            width="16"
+            height="16"
+            fill="currentColor"
+            aria-hidden="true"
+            class="xfx01vb x1lliihq x1tzjh5l x1k90msu x2h7rmj x1qfuztq"
+            style="--color: var(--primary-icon)"
+          >
+            <g fill-rule="evenodd" transform="translate(-450 -1073)">
+              <path
+                d="m459.603 1077.948-1.762 2.851a.89.89 0 0 1-1.302.245l-1.402-1.072a.354.354 0 0 0-.433.001l-1.893 1.465c-.253.196-.583-.112-.414-.386l1.763-2.851a.89.89 0 0 1 1.301-.245l1.402 1.072a.354.354 0 0 0 .434-.001l1.893-1.465c.253-.196.582.112.413.386M456 1073.5c-3.38 0-6 2.476-6 5.82 0 1.75.717 3.26 1.884 4.305.099.087.158.21.162.342l.032 1.067a.48.48 0 0 0 .674.425l1.191-.526a.473.473 0 0 1 .32-.024c.548.151 1.13.231 1.737.231 3.38 0 6-2.476 6-5.82 0-3.344-2.62-5.82-6-5.82"
+              ></path>
+            </g>
+          </svg>
+          <p class="whitespace-nowrap font-semibold">Send message</p>
+        </div>
+      </div>
+
+      <div class="mx-3 border-b py-3 flex justify-between items-center">
+        <div class="flex items-center gap-2">
+          <img class="w-[18px] h-[18px]" src="/like.svg" alt="" />
+          <p class="text-[#65676B]">696</p>
+        </div>
+
+        <div class="flex gap-3 items-center">
+          <p class="text-[#65676B] text-[15px]">3 comments</p>
+          <p class="text-[#65676B] text-[15px]">9 shares</p>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-2 mx-3 my-1">
+        <div class="flex justify-center items-center gap-2 p-2 hover:bg-[#f1f2f5]">
+          <i
+            class="fillter-custom"
+            data-visualcompletion="css-img"
+            style="
+              background-image: url(/list-icon.png);
+              background-position: 0px -1014px;
+              background-size: 33px 1413px;
+              width: 18px;
+              height: 18px;
+              background-repeat: no-repeat;
+              display: inline-block;
+            "
+          ></i>
+          <p class="text-[#65676B]">Like</p>
+        </div>
+        <div class="flex justify-center items-center gap-2 p-2 hover:bg-[#f1f2f5]">
+          <i
+            class="fillter-custom"
+            data-visualcompletion="css-img"
+            style="
+              background-image: url('/list-icon.png');
+              background-position: 0px -919px;
+              background-size: 33px 1413px;
+              width: 18px;
+              height: 18px;
+              background-repeat: no-repeat;
+              display: inline-block;
+            "
+          ></i>
+          <p class="text-[#65676B]">Like</p>
+        </div>
+      </div>
+
+      <!-- comments -->
+      <div class="mx-3 border-t"></div>
+    </div>
   </div>
 </template>
 
+<style scoped>
+.custom-shadow {
+  box-shadow:
+    0 12px 28px 0 rgba(0, 0, 0, 0.2),
+    0 2px 4px 0 rgba(0, 0, 0, 0.1),
+    inset 0 0 0 1px rgba(255, 255, 255, 0.5);
+}
+
+.fillter-custom {
+  filter: invert(39%) sepia(21%) saturate(200%) saturate(109.5%) hue-rotate(174deg) brightness(94%)
+    contrast(86%);
+}
+</style>
